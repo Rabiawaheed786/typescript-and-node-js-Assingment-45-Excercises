b96afc184e2893eef71dcfbb2ee96f9b9ab76dab
Rabiawaheed786 <162490318+Rabiawaheed786@users.noreply.github.com>
2024-07-14 03:40:16 +0400

    Add files via upload

---
+++ Ex.22step21/main.js
@@ -0,0 +1,6 @@
+//creating a Array
+var errorArray = ["A", "B", "c", "D"];
+//producing Error! by accessing invalid index of array
+//console.log(errorArray[10]);
+//ERor remove
+console.log(errorArray[1]);
---
+++ Ex.22step21/main.ts
@@ -0,0 +1,8 @@
+//creating a Array
+let errorArray: string [] =  ["A","B","c","D"];
+
+//producing Error! by accessing invalid index of array
+
+//console.log(errorArray[10]);
+//Eror Removed
+ console.log(errorArray[1]);
