54574b2c6f43eed2e90e925a9e2874b6e2c80f07
Rabiawaheed786 <162490318+Rabiawaheed786@users.noreply.github.com>
2024-07-14 03:44:20 +0400

    Add files via upload

---
+++ Ex26step25/main.js
@@ -0,0 +1,13 @@
+//Define variable
+var aliencolor = "green";
+//if(aliencolor === "green"){
+//console.log("player just earned 5 points for shooting the alien.");
+//else{
+// console.log("player just earned 10 points.");
+//second version
+if (aliencolor === "red") {
+    console.log("I am come from If statement. ");
+}
+else {
+    console.log("I am come from Else statement .");
+}
---
+++ Ex26step25/main.ts
@@ -0,0 +1,19 @@
+//Define variable
+let aliencolor = "green";
+
+ //if(aliencolor === "green"){
+
+//console.log("player just earned 5 points for shooting the alien.");
+
+ 
+ //else{
+   // console.log("player just earned 10 points.");
+ 
+ //second version
+ if(aliencolor === "red"){
+    console.log("I am come from If statement. ");
+ }
+ else{
+    console.log("I am come from Else statement .");
+    
+ }
